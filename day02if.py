90181ccce05c11657c75fed884f17b3de11ed7d6
[leedabin2] <12193152@inha.edu>
2023-01-13 16:18:29 +0900

    day02 4.2

--- day02if.py
+++ day02if.py
@@ -12,6 +12,25 @@ elif guess == secret :
 else:
     print("too high")
 
+#4.2
+
+small = True
+green = False
+
+if small :
+    if green:
+        print("완두콩은 작고 녹색이다")
+    else:
+        print("체리는 작고 녹색이 아니다")
+else:
+    if green:
+        print("수박은 크고 녹색이다")
+    else:
+        print("호박은 크고 녹색이 아니다")
+
+
+
+
 
 
 
