fe76aa981f2d03ecc08d8ba94b1a00289ebb6ed6
[leedabin2] <12193152@inha.edu>
2023-01-13 14:04:32 +0900

    day02 numbers

---
+++ day02.py
@@ -0,0 +1,26 @@
+#chap 3
+
+#number = 0b10011010
+#number = 0x9a
+#number = 0o232
+#print(number)
+
+number = 154
+print(bin(number))
+print(hex(number))
+print(oct(number))
+
+print(ord(" ")) # ascii
+print(hex(ord(" "))) # 20
+
+
+
+
+
+
+
+
+
+
+
+
