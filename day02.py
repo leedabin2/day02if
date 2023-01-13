58fad13586c293060a9ebd7f8b3ad808e39fb48b
[leedabin2] <12193152@inha.edu>
2023-01-13 15:27:37 +0900

    day02 if random

--- day02.py
+++ day02.py
@@ -1,11 +1,12 @@
 #chap 4 if
+import random
 
 limits = 20
-tweets = "pass" * 4
+tweets = "pass" * random.randint(1, 10) # 1에서 10 사이의 정수가 임의로 발생
 diff = limits - len(tweets)
 if diff >= 0:
     print(tweets)
-else
+else:
     print(f'글자 수 {abs(diff)}초과')
 
 
